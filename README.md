68eb6cd815742fac8b8e4775d13aca9f73d99057
WooKyoung Noh <wookay.noh@gmail.com>
2021-09-10 14:14:01 +0900

    yannic

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # UnsplashImages.jl
 
+some test images from <https://unsplash.com/>
+
+```
+julia> using UnsplashImages
+
+julia> img = unsplash_image("yannic-laderach-Dqx4XWuXu7w-unsplash.small.jpg")
+
+julia> size(img)
+(800, 640)
+```
 
 
 ### borrowed code in the `main` branch from TestImages.jl
--- test/unsplashimages/unsplash_image.jl
+++ test/unsplashimages/unsplash_image.jl
@@ -3,7 +3,13 @@ module test_unsplashimages_unsplash_image
 using Test
 using UnsplashImages
 
-img = unsplash_image("tyler-nix-k5R4cdIAiuk-unsplash.small.jpg")
-@test size(img) == (960, 640)
+yannic= unsplash_image("yannic-laderach-Dqx4XWuXu7w-unsplash.small.jpg")
+@test size(yannic) == (800, 640)
+
+yannic_mask = unsplash_image("yannic-laderach-Dqx4XWuXu7w-unsplash.small.u2net_human_seg.png")
+@test size(yannic_mask) == (800, 640)
+
+tyler = unsplash_image("tyler-nix-k5R4cdIAiuk-unsplash.small.jpg")
+@test size(tyler) == (960, 640)
 
 end # module test_unsplashimages_unsplash_image
